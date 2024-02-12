d08cd56cd11fab60354f1d5c16a6fc62480b7748
nerubashka <50240702+nerubashka@users.noreply.github.com>
2024-02-12 05:05:50 +0300

    webpack 1.1.2
    Изменены настройки webpack для сервера

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const PORT = 8069
 
-connect()
-    .use(serveStatic(__dirname))
-    .listen(8069, () => console.log('Server running on 8069...'));
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Expires': '0'
+    });
+    fs.createReadStream(path.resolve(__dirname, 'src/index.html')).pipe(res)
+})
 
+server.listen(PORT, '0.0.0.0', () => {
+    console.log('Server started!')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,47 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+
+//const isDev = process.env.NODE_ENV === 'development'
+//const isProd = !isDev
+
+const plugins = () => {
+    const base = [
+        new HTMLWebpackPlugin({
+            template: './intMap.html',
+            filename: 'intMap.html',
+            chunks: ['main']
+        }),
+        new HTMLWebpackPlugin({ 
+            template: './index.html',
+            filename: 'index.html',
+            chunks: ['sphere']
+        }),
+        new CleanWebpackPlugin(),
+    ]
+
+    //if (isProd) {
+    //    base.push(new BundleAnalyzerPlugin())
+    //}
+
+    return base
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        //imports: './scripts/imports.js',
         main: './global_main.js',
-        //features: './scripts/drawFeatures.js',
-        //images: './scripts/satimages.js',
+        sphere: './sphere.js'
     },
     output: {
+        publicPath: '/',
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: false
     },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './intMap.html',
-            filename: 'intMap.html'
-        }),
-        new CleanWebpackPlugin(),
-    ],
+    plugins: plugins(),
     module: {
         rules: [
             {
@@ -33,5 +53,17 @@ module.exports = {
                 type: 'asset/resource'
             }
         ]
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname),
+        },
+        port: 8069,
+        open: true
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     }
 }
